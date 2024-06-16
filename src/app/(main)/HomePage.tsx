796a59e67d34faa848cc5becd4966c86a92a1edb
Miles Wiesenthal <mileswiesenthal@gmail.com>
2024-06-15 22:13:47 -0700

    more padding

--- src/app/(main)/HomePage.tsx
+++ src/app/(main)/HomePage.tsx
@@ -8,7 +8,7 @@ import Download from "../download/download";
 export default function HomePage() {
   const { page } = usePageNavigator();
   return (
-    <div className="size-full flex flex-col justify-start items-center px-4 md:px-8 py-4">
+    <div className="size-full flex flex-col justify-start items-center px-4 py-4 pt-8 md:px-8">
       {page === Page.Home && <Home />}
       {page === Page.Upload && <Upload />}
       {page === Page.Download && <Download />}
